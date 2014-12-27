b124c2174abf4402cc439142ba5687de45ec99f4
Nandan Rao <nandanmarkrao@gmail.com>
2014-12-27 12:49:24 -0500

    added fonts and began font styling

--- www/lib/templates/templates.js
+++ www/lib/templates/templates.js
@@ -14,21 +14,21 @@ try { module = angular.module("ngTemplates"); }
 catch(err) { module = angular.module("ngTemplates", []); }
 module.run(["$templateCache", function($templateCache) {
   $templateCache.put("collaboration/myCollaborations.html",
-    "<ion-view>\n" +
-    "<ion-content>\n" +
-    "this is my collaborattions\n" +
-    "<button class=\"button button-block button-energized button-outline\" ng-click=\"myCollaborations.logout()\">Logout</button>\n" +
-    "<p>{{ user.email }}</p>\n" +
-    "<p>{{ user.displayName }}</p>\n" +
+    "<ion-view view-title=\"mis collaboraciones\">\n" +
+    "	<ion-nav-buttons side=\"left\">\n" +
+    "	 <button menu-toggle=\"left\" class=\"button button-icon icon ion-navicon\"></button>\n" +
+    "	</ion-nav-buttons>\n" +
+    "	<ion-content>\n" +
+    "	<button class=\"button button-block button-energized button-outline\" ng-click=\"myCollaborations.logout()\">Logout</button>\n" +
     "\n" +
-    "<div ng-repeat=\"collaboration in collaborations\">\n" +
-    "	<div class=\"collaboration\">\n" +
-    "		<button class=\"button button-block button-balanced button-outline\" ng-click=\"myCollaborations.collaboration(collaboration.$id)\">{{ collaboration.name }}</button>\n" +
+    "	<div ng-repeat=\"collaboration in collaborations\">\n" +
+    "		<div class=\"collaboration\">\n" +
+    "			<button class=\"button button-block button-balanced button-outline\" ng-click=\"myCollaborations.collaboration(collaboration.$id)\">{{ collaboration.name }}</button>\n" +
+    "		</div>\n" +
     "	</div>\n" +
-    "</div>\n" +
     "\n" +
-    "<button ng-click=\"myCollaborations.newCollaboration()\">new collaboration</button>\n" +
-    "</ion-content>\n" +
+    "	<button class=\"newCollaboration\" ng-click=\"myCollaborations.newCollaboration()\">+</button>\n" +
+    "	</ion-content>\n" +
     "</ion-view>");
 }]);
 })();
@@ -155,27 +155,27 @@ try { module = angular.module("ngTemplates"); }
 catch(err) { module = angular.module("ngTemplates", []); }
 module.run(["$templateCache", function($templateCache) {
   $templateCache.put("login/login.html",
-    "<div style=\"padding: 0 10px\">\n" +
-    "<p>hey this is login</p>\n" +
-    "<button class=\"button button-block button-positive button-outline\" ng-click=\"login.authLogin('facebook')\">Facebook</button>\n" +
-    "<button class=\"button button-block button-assertive button-outline\" ng-click=\"login.authLogin('google')\">Googllle</button>\n" +
-    "<button class=\"button button-block button-assertive button-outline\" ng-if=\"!local\" ng-click=\"login.showLocalLogin()\">Email</button>\n" +
-    "<form name=\"blah\"></form>\n" +
-    "<div ng-show=\"local\" class=\"list\">\n" +
-    "	<form name=\"loginForm\" no-validate class=\"localLoginForm\" ng-submit=\"login.login()\">\n" +
-    "		<label class=\"item item-input\">\n" +
-    "			<input required type=\"text\" ng-model=\"form.email\" class=\"email\" name=\"email\" placeholder=\"Email\"></input>\n" +
-    "		</label>\n" +
-    "		<label class=\"item item-input\">\n" +
-    "			<input required type=\"text\" ng-model=\"form.password\" class=\"password\" name=\"password\" placeholder=\"Password\"></input>\n" +
-    "		</label>\n" +
-    "		<label ng-if=\"register\" class=\"item item-input\">\n" +
-    "			<input type=\"text\" ng-model=\"form.passRepeat\" class=\"repeatPass\" name=\"repeatPass\" placeholder=\"Repeat Password\"></input>\n" +
-    "		</label>\n" +
-    "		<button required ng-if=\"!register\" class=\"button button-outline button-positive login\" type=\"submit\" >Login</button>\n" +
-    "		<button type=\"button\" ng-click=\"login.register()\" class=\"button button-outline button-positive register\">Register</button>\n" +
-    "	</form>\n" +
-    "</div>");
+    "<ion-content>\n" +
+    "	<button class=\"button button-block button-positive button-outline\" ng-click=\"login.authLogin('facebook')\">Facebook</button>\n" +
+    "	<button class=\"button button-block button-assertive button-outline\" ng-click=\"login.authLogin('google')\">Googllle</button>\n" +
+    "	<button class=\"button button-block button-assertive button-outline\" ng-if=\"!local\" ng-click=\"login.showLocalLogin()\">Email</button>\n" +
+    "	<form name=\"blah\"></form>\n" +
+    "	<div ng-show=\"local\" class=\"list\">\n" +
+    "		<form name=\"loginForm\" no-validate class=\"localLoginForm\" ng-submit=\"login.login()\">\n" +
+    "			<label class=\"item item-input\">\n" +
+    "				<input required type=\"text\" ng-model=\"form.email\" class=\"email\" name=\"email\" placeholder=\"Email\"></input>\n" +
+    "			</label>\n" +
+    "			<label class=\"item item-input\">\n" +
+    "				<input required type=\"text\" ng-model=\"form.password\" class=\"password\" name=\"password\" placeholder=\"Password\"></input>\n" +
+    "			</label>\n" +
+    "			<label ng-if=\"register\" class=\"item item-input\">\n" +
+    "				<input type=\"text\" ng-model=\"form.passRepeat\" class=\"repeatPass\" name=\"repeatPass\" placeholder=\"Repeat Password\"></input>\n" +
+    "			</label>\n" +
+    "			<button required ng-if=\"!register\" class=\"button button-outline button-positive login\" type=\"submit\" >Login</button>\n" +
+    "			<button type=\"button\" ng-click=\"login.register()\" class=\"button button-outline button-positive register\">Register</button>\n" +
+    "		</form>\n" +
+    "	</div>\n" +
+    "</ion-content>");
 }]);
 })();
 
