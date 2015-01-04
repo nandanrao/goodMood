54f3e362bc14b69ee52893a6d997c4d63ae5cbcc
Nandan Rao <nandanmarkrao@gmail.com>
2015-01-04 10:21:11 -0500

    fixed facebook image ref

--- www/js/facebook/facebook.service.js
+++ www/js/facebook/facebook.service.js
@@ -18,7 +18,9 @@ angular.module('goodMood')
 		}
 		
 		Facebook.getPicture = function(){
-			return $cordovaFacebook.api('me?fields=picture.type(large)')
+			return $cordovaFacebook.api('me?fields=picture.type(large)').then(function(obj){
+				return obj.data.url
+			})
 	  }
 
 	  Facebook.getLoginStatus = function(){
