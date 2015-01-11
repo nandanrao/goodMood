1dbd4fa4d8a7548573db7ef6c2dd7c45e0427636
Nandan Rao <nandanmarkrao@gmail.com>
2015-01-11 01:30:43 -0500

    housekeeping

---
+++ www/js/facebook/fb.js
@@ -0,0 +1,17 @@
+angular.module('goodMood')	
+	.factory('fb', function(){
+
+		var fb = {};
+
+		fb.ref = new Firebase('https://goodmood.firebaseio.com')
+
+		fb.users = fb.ref.child('users');
+		fb.threads = fb.ref.child('threads');
+		fb.iterations = fb.ref.child('iterations');
+		fb.collaborations = fb.ref.child('collaborations');
+		fb.messages = fb.ref.child('messages');
+		fb.images = fb.ref.child('images');
+		fb.audio = fb.ref.child('audio');
+		
+		return fb
+	})
